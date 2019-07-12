661e459ada1944c7036f1a51dcee058485b209de
gorillabull <thisisntlongonlytomakelifehard@gmail.com>
2019-07-12 10:49:45 -0700

    finished 2d ufo game tutorial

--- unity1/New Unity Project (2)/Assets/CharacterCOntroller2D.cs
+++ unity1/New Unity Project (2)/Assets/CharacterCOntroller2D.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class CharacterCOntroller2D : MonoBehaviour
 {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    public Text countText;
+    public Text WinText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        WinText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveH, moveV);
+        rb2d.AddForce(movement * speed);
+    }
+ 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count>=5)
+        {
+            WinText.text = "Win!";
+        }
     }
 }
