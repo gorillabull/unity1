6547446d4de8fc21b127954fae462098b4864059
Leonardo  Vasillev <vasilevl@ad.cs.unlv.edu>
2019-07-16 14:00:13 -0700

    added bezier curves

---
+++ unity1/New Unity Project (2)/Assets/New Folder/Editor/LineInspector.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnSceneGUI()
+    {
+        Line line = target as Line;
+        Transform handleTransform = line.transform;
+        Quaternion handleRot = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        Handles.color = Color.white;
+        Handles.DrawLine(p0, p1);
+        EditorGUI.BeginChangeCheck();
+        p0= Handles.DoPositionHandle(p0, handleRot);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+        EditorGUI.BeginChangeCheck();
+        p1= Handles.DoPositionHandle(p1, handleRot);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+
+
+    }
+}
